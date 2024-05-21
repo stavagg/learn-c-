52f1e829bbb529cf57e1731e386e633cfa8404f4
stavagg <barinovs141@gmail.com>
2024-05-21 20:25:00 +0300

    Blya ia ahuel

--- learn-c#/learn-c#/Program.cs
+++ learn-c#/learn-c#/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace learn_c_
+﻿/******************************************************************************
+создать и добавить в класс "дебетовая карта" ифнормацию о сумме денег на карте. Выполните перегрузку операторов: 
++(увеличение суммы на указанную сумму),
+-(для уменьшения суммы денег на указанное число),
+==(для проверки равенства cvc кода),
+< и >,
+!=
+использовать механизм свойств для полей класса.
+*******************************************************************************/
+namespace learn_c_
 {
     internal class Program
     {
+        class Debit
+        {
+
+            public void Print()
+            {
+                Console.WriteLine($"Amount is: {money}");
+            }
+            public double money { get; set; }
+            public double number {  get; set; }
+        public static Debit operator +(Debit c1, double number)
+        {
+            c1.money += number;
+            return c1;
+        }
+        public static Debit operator -(Debit c1, double number)
+        {
+            c1.money -= number;
+            return c1;
+        }
+        }
+
+        /*
+
+        public static bool operator ==(Debit c1, Debit c2)
+        {
+            return c1.cvv == c2.cvv;
+        }
+        public static bool operator !=(Debit c1, Debit c2)
+        {
+            return c1.cvv != c2.cvv;
+        }
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Debit d1 = new Debit { money = 111, number = 100 };
+            d1.Print();
+            Debit d2 = new Debit { money = 222, number = 50 };
+            d1 = d1 +  45;
+            d1.Print();
         }
     }
 }
--- learn-c#/learn-c#/obj/Debug/net8.0/learn-c#.AssemblyInfo.cs
+++ learn-c#/learn-c#/obj/Debug/net8.0/learn-c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn-c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3274d1843245ecd9705b1cdd7db41ba978cb7ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed13c7f606daaf3b1fcbcd1ad2208e36dc5efca8")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn-c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn-c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
