48db1e6e3de84034a05f9033a90977fed173f874
stavagg <barinovs141@gmail.com>
2024-05-28 20:09:14 +0300

    Простенький конструктор тортов

---
+++ inheritance-04/inheritance-04/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace inheritance_04
+{
+    internal class Program
+    {
+        class Tort
+        {
+            public bool _chocolate;
+            public bool _cream;
+            int _layers;
+            int _candles;
+            string _wishes;
+            public Tort(bool chocolate, bool cream, int layers, int candles, string wishes)
+            {
+                _chocolate = chocolate;
+                _cream = cream;
+                _layers = layers;
+                _candles = candles;
+                _wishes = wishes;
+            }
+            public void print(Tort tort)
+            {
+                Console.WriteLine($"Наличие шоколада: {_chocolate}\n Наличие крема: {_cream}\nКоличество слоев: {_layers}\nКоличество свечей: {_candles}\n " +
+                    $"Пожелания клиента: {_wishes}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var tort = new Tort( true, false, 1, 3, "делайте хорошо плохо не делайте" );
+            tort.print(tort);
+        }
+    }
+}
