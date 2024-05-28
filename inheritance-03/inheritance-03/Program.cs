cb43da0c5e1ffd8e832e5982b34a9de1c0c1d72e
stavagg <barinovs141@gmail.com>
2024-05-28 18:30:19 +0300

    Пример наследования

---
+++ inheritance-03/inheritance-03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace inheritance_03
+{
+    class Program
+    {
+        public abstract class Animal
+        {
+            readonly string _name;
+            readonly string _characteristic;
+
+            protected Animal(string name, string characteristic)
+            {
+                _name = name;
+                _characteristic = characteristic;
+            }
+
+            public abstract void Voice();
+
+            public override string ToString()
+            {
+                return $"Animal {_name} - {_characteristic}";
+            }
+        }
+
+        class Tiger : Animal
+        {
+            public Tiger(string name, string characteristic) : base(name, characteristic) { }
+
+            public override void Voice() 
+            {
+                Console.WriteLine("rrrrrrrrr");
+            }
+        }
+
+        class Crocodile : Animal
+        {
+            public Crocodile(string name, string characteristic) : base(name, characteristic) { }
+            public override void Voice()
+            {
+                Console.WriteLine("klac-klac");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var tiger = new Tiger("Sasha", "Big cat");
+            var croco = new Crocodile("Fedya", "Zeleniy chel");
+
+            tiger.Voice();
+            Console.WriteLine(tiger.ToString());
+
+            croco.Voice();
+            Console.WriteLine(croco.ToString());
+        }
+    }
+}
